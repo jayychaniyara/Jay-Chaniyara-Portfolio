465aef9a72d10404ef55c03623ccdddfd19fed34
jayychaniyara <chaniyarajay007@gmail.com>
2025-04-19 18:05:52 +0530

    Repository name updated

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/jay-chaniyara-portfolio">
+      <BrowserRouter basename="/Jay-Chaniyara-Portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/jay-chaniyara-portfolio/',
+  base: '/Jay-Chaniyara-Portfolio/',
   server: {
     host: "localhost",
     port: 8080,
