ad20a6dec89fb4343e2063af047cebb9fd7412d3
jayychaniyara <chaniyarajay007@gmail.com>
2025-09-17 19:10:46 +0530

    Updated experience

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
           <div className="lg:col-span-7 space-y-6 animate-fade-in">
             <p className="text-lg leading-relaxed">
               I'm a results-driven and detail-oriented full-stack developer with
-              over 3+ years of experience in building scalable web applications.
+              over 3.5+ years of experience in building scalable web applications.
               My expertise lies in working with Angular, .NET Core, MVC, Web
               API, and SQL Server.
             </p>
